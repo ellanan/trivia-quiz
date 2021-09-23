3727672f3a5c202f44c69ab98d02a79fa4c9fe0f
Ella N <38847123+ellanan@users.noreply.github.com>
2021-09-23 17:28:03 -0400

    install emotion/styled css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import React from 'react';
+
+import { questions } from './questions';
+
 function App() {
   return (
     <div>
       <h1>TriviaQuiz</h1>
+      <p>Score: 0</p>
+      <button>Start</button>
+      <>
+        {questions.map((question, index) => {
+          return (
+            <ul>
+              <li>
+                {question.question}
+                {questions.map((question, index) => {
+                  return (
+                    <ul>
+                      <li>{question.possibleAnswers[index]}</li>
+                      <li>{question.possibleAnswers[index + 1]}</li>
+                      <li>{question.possibleAnswers[index + 2]}</li>
+                      <li>{question.possibleAnswers[index + 3]}</li>
+                    </ul>
+                  );
+                })}
+              </li>
+            </ul>
+          );
+        })}
+      </>
+      <button>Next Question</button>
+      <button>Close</button>
     </div>
   );
 }
