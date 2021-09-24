b671ac562b36befe0220429007d750eda90e1c67
Ella N <38847123+ellanan@users.noreply.github.com>
2021-09-24 13:36:35 -0400

    add button component

---
+++ src/Buttons.ts
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #665cde;
+  border: none;
+  border-radius: 10px;
+  padding: 6px 12px;
+  min-width: 60px;
+  color: #fff;
+`;
+
+export const ButtonAnswers = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #cec4f0;
+  border: none;
+  border-radius: 10px;
+  padding: 6px 12px;
+`;
