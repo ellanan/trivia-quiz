141732c9104f9a5fc59fe0ede4eea0f828551ff5
Ella N <38847123+ellanan@users.noreply.github.com>
2021-09-28 14:29:08 -0400

    update button component

--- src/components/Buttons.ts
+++ src/components/Buttons.ts
@@ -1,18 +1,6 @@
 import styled from '@emotion/styled';
 
-export const Button = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #665cde;
-  border: none;
-  border-radius: 10px;
-  padding: 6px 12px;
-  min-width: 60px;
-  color: #fff;
-`;
-
-export const ButtonAnswers = styled.button`
+export const PossibleAnswerButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -20,6 +8,45 @@ export const ButtonAnswers = styled.button`
   border: none;
   border-radius: 10px;
   margin: 10px;
-  padding: 6px 12px;
-  min-width: 260px;
+  padding: 0.7em 0;
+  min-width: 300px;
+  font-size: 1em;
+  cursor: pointer;
+  color: #4940b6;
+  font-weight: 600;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #665cde;
+  border: none;
+  border-radius: 10px;
+  padding: 0.7em 1.3em;
+  min-width: 60px;
+  color: #fff;
+  font-size: 1em;
+  margin-top: 2em;
+  cursor: pointer;
+  font-weight: 600;
+  &[disabled] {
+    background-color: #8e7c7cb3;
+  }
+`;
+
+export const StartButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #665cde;
+  border: 3px solid #fff;
+  border-radius: 10px;
+  margin: 20px;
+  padding: 0.7em 0.5em;
+  min-width: 100px;
+  color: #fff;
+  font-size: 1.2em;
+  font-weight: 600;
+  cursor: pointer;
 `;
